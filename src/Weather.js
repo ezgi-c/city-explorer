96edfe6f4f75b478cc7a3c7dd81921258bfa3515
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-10-10 17:30:09 -0700

    completed errors task for lab 7, app displays error message for API server errors

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ class Weather extends React.Component {
   render() {
     return (
       <>
-        <Accordion defaultActiveKey="0">
+        <Accordion defaultActiveKey="0"> 
             <Accordion.Item eventKey="0" >
               <Accordion.Header>{this.props.date}</Accordion.Header>
               <Accordion.Body>{this.props.description}</Accordion.Body>
