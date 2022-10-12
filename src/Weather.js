5ddaf11ec81c73a008ee7e9ae1ebe12f116bbd0e
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-10-11 20:58:55 -0700

    refactor

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,12 @@
 import "./App.css";
 import React from "react";
-import Accordion from "react-bootstrap/Accordion";
 
 class Weather extends React.Component {
   render() {
     return (
       <>
-        <Accordion defaultActiveKey="0"> 
-            <Accordion.Item eventKey="0" >
-              <Accordion.Header>{this.props.date}</Accordion.Header>
-              <Accordion.Body>{this.props.description}</Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
+        <h3>{this.props.date}</h3>
+        <h4>{this.props.description}</h4>
       </>
     );
   }
