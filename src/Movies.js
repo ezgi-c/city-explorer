b53f4dff124edaa7e8048c22eee87936aedf5a7e
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-10-14 10:43:12 -0700

    rendered out movies without images

--- src/Movies.js
+++ src/Movies.js
@@ -5,15 +5,21 @@ class Movies extends React.Component {
   render() {
     return (
       <Row>
-        {this.props.movies.map((movie) => (
+        
+        {this.props.movies.map((movie, idx) => (movie.poster_path !== null &&
           <Col>
-            <Card>
+            <Card key={idx}>
               <Card.Img 
               src = {movie.image_url}
               alt= {movie.overview}
               />
               <Card.Body>
                 <Card.Text>Title:{movie.title}</Card.Text>
+                <Card.Text>Overview:{movie.overview}</Card.Text>
+                <Card.Text>Average Votes:{movie.average_votes}</Card.Text>
+                <Card.Text>Total Votes:{movie.total_votes}</Card.Text>
+                <Card.Text>Popularity:{movie.popularity}</Card.Text>
+                <Card.Text>Released on:{movie.released_on}</Card.Text>
               </Card.Body>
             </Card>
           </Col>
